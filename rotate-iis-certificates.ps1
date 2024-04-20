18295e24f3082d6a1f7b78c04dac8376fac89bb0
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-04-20 20:32:44 +0100

    Update ACMESharp module installation and add logging statements in rotate-iis-certificates.ps1

--- rotate-iis-certificates.ps1
+++ rotate-iis-certificates.ps1
@@ -6,7 +6,7 @@ Write-Host "Rotating IIS certificates on $env:COMPUTERNAME"
 
 # Install required modules
 Get-PSRepository | Write-Host
-Find-Module -Name ACMESharp | Install-Module -Scope CurrentUser -AcceptLicense
+Find-Module -Name ACMESharp | Install-Module -Scope CurrentUser -AcceptLicense -Confirm:$False -Force
 
 # Rotate certificates for required bindings
 $bindings | ForEach-Object {
