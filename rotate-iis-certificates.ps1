4e7bca8eb722499c0fa8434f3caf853167871b46
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-04-20 20:46:56 +0100

    Update Posh-ACME module installation and add logging statements in rotate-iis-certificates.ps1

--- rotate-iis-certificates.ps1
+++ rotate-iis-certificates.ps1
@@ -6,8 +6,8 @@ Write-Host "Rotating IIS certificates on $env:COMPUTERNAME"
 
 # Install and import required modules
 Get-PSRepository | Write-Host
-Find-Module -Name ACMESharp | Install-Module -Scope CurrentUser -AcceptLicense -Confirm:$False -Force
-Import-Module ACMESharp
+Find-Module -Name Posh-ACME | Install-Module -Scope CurrentUser -AcceptLicense -Confirm:$False -Force
+Import-Module Posh-ACME
 
 # Rotate certificates for required bindings
 $bindings | ForEach-Object {
