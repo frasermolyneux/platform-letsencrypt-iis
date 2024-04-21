c40d94342e3b8bec970fad3d8a02b5a48718fd56
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-04-21 12:33:34 +0100

    Fix import statement for IISAdministration module

--- rotate-iis-certificates.ps1
+++ rotate-iis-certificates.ps1
@@ -22,6 +22,7 @@ if (-not (Test-Path $env:POSHACME_HOME)) {
 # Import required modules
 Import-Module Posh-ACME
 Import-Module Posh-ACME.Deploy
+Import-Module IISAdministration -AllowClobber -Force
 Import-Module WebAdministration
 
 # Use Lets Encrypt Staging environment
